d4eea5d770e008e73e1200fee662e894890154f1
Seungjin Kim <seungjin429@gmail.com>
2023-05-25 13:39:13 +0900

    meta 데이터 이슈 적용

---
+++ blog/pages/_document.tsx
@@ -0,0 +1,29 @@
+import { CONFIG } from 'config'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang={CONFIG.lang}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="/apple-touch-icon.png"></link>
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            title="RSS 2.0"
+            href="/feed"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
