1571f7c0f1d6afebb33460d1d01c7edbd6ca22aa
Seungjin Kim <seungjin429@gmail.com>
2023-07-12 17:42:43 +0900

    _app 셋팅 리펙토링

--- blog/pages/_app.tsx
+++ blog/pages/_app.tsx
@@ -2,11 +2,12 @@ import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { ReactElement, ReactNode, useEffect, useState } from 'react'
 /** react-notion-x */
-import { QueryClient } from '@tanstack/react-query'
 import 'katex/dist/katex.min.css'
 import 'prismjs/themes/prism-tomorrow.css'
 import 'react-notion-x/src/styles.css'
-import { AppRegister } from 'src/components/AppRegister'
+import { usePageLoading } from 'src/common/hooks/useLoading'
+import AppRegister from 'src/components/AppRegister'
+import PageSpinner from 'src/components/@common/spinner/PageSpinner'
 
 export type NextPageWithLayout<PageProps = {}> = NextPage<PageProps> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -18,15 +19,20 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || (page => page)
-  const [domLoaded, setDomLoaded] = useState(false)
+  const [isDomLoaded, setIsDomLoaded] = useState(false)
+  const { isLoading } = usePageLoading()
 
   useEffect(() => {
-    setDomLoaded(true)
+    setIsDomLoaded(true)
   }, [])
 
   return (
     <>
-      {domLoaded && <AppRegister>{getLayout(<Component {...pageProps} />)}</AppRegister>}
+      {isDomLoaded && (
+        <AppRegister>
+          {isLoading ? <PageSpinner /> : getLayout(<Component {...pageProps} />)}
+        </AppRegister>
+      )}
     </>
   )
 }
