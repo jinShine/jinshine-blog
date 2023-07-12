f1893e2f624b92acf17a5b9bdd01a8ef708c461a
Seungjin Kim <seungjin429@gmail.com>
2023-07-12 17:42:24 +0900

    useRedirect 개발

---
+++ blog/src/common/hooks/useRedirect.ts
@@ -0,0 +1,10 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+export const useRedirect = (path: string) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push(path)
+  }, [router, path])
+}
